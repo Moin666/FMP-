a9eec8d254d2936393a61d2f84a5eaaad612e586
MoinKhan <moin.khan.mk6600@gmail.com>
2022-01-30 14:00:49 +0500

    Shop By Brand Section Complete

---
+++ app.js
@@ -0,0 +1,127 @@
+var rolexOyster = document.querySelectorAll(".rolexOyster")
+var rolexTxt = document.querySelectorAll(".rolexTxt")
+var pricerolex = document.querySelectorAll(".price")
+var imgLinks = ['images/body/landscapeMode.jpg','images/landscapeMode.jpg']
+
+
+function changeImagesbtnClick1(){
+    rolexOyster[0].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[1].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[2].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[3].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[4].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[5].setAttribute('src','images/landscapeMode.jpg')
+    rolexTxt[0].innerHTML = "text provided by user";
+    rolexTxt[1].innerHTML = "text provided by user";
+    rolexTxt[2].innerHTML = "text provided by user";
+    rolexTxt[4].innerHTML = "text provided by user";
+    rolexTxt[5].innerHTML = "text provided by user";
+    pricerolex[0].innerHTML = "text provided by user";
+    pricerolex[1].innerHTML = "text provided by user";
+    pricerolex[2].innerHTML = "text provided by user";
+    pricerolex[3].innerHTML = "text provided by user";
+    pricerolex[4].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    console.log(pricerolex)
+}
+function changeImagesbtnClick2(){
+    rolexOyster[0].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[1].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[2].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[3].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[4].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[5].setAttribute('src','images/landscapeMode.jpg')
+    rolexTxt[0].innerHTML = "text provided by user";
+    rolexTxt[1].innerHTML = "text provided by user";
+    rolexTxt[2].innerHTML = "text provided by user";
+    rolexTxt[4].innerHTML = "text provided by user";
+    rolexTxt[5].innerHTML = "text provided by user";
+    pricerolex[0].innerHTML = "text provided by user";
+    pricerolex[1].innerHTML = "text provided by user";
+    pricerolex[2].innerHTML = "text provided by user";
+    pricerolex[3].innerHTML = "text provided by user";
+    pricerolex[4].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    console.log(pricerolex)
+}
+function changeImagesbtnClick3(){
+    rolexOyster[0].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[1].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[2].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[3].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[4].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[5].setAttribute('src','images/landscapeMode.jpg')
+    rolexTxt[0].innerHTML = "text provided by user";
+    rolexTxt[1].innerHTML = "text provided by user";
+    rolexTxt[2].innerHTML = "text provided by user";
+    rolexTxt[4].innerHTML = "text provided by user";
+    rolexTxt[5].innerHTML = "text provided by user";
+    pricerolex[0].innerHTML = "text provided by user";
+    pricerolex[1].innerHTML = "text provided by user";
+    pricerolex[2].innerHTML = "text provided by user";
+    pricerolex[3].innerHTML = "text provided by user";
+    pricerolex[4].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    console.log(pricerolex)
+}
+function changeImagesbtnClick4(){
+    rolexOyster[0].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[1].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[2].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[3].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[4].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[5].setAttribute('src','images/landscapeMode.jpg')
+    rolexTxt[0].innerHTML = "text provided by user";
+    rolexTxt[1].innerHTML = "text provided by user";
+    rolexTxt[2].innerHTML = "text provided by user";
+    rolexTxt[4].innerHTML = "text provided by user";
+    rolexTxt[5].innerHTML = "text provided by user";
+    pricerolex[0].innerHTML = "text provided by user";
+    pricerolex[1].innerHTML = "text provided by user";
+    pricerolex[2].innerHTML = "text provided by user";
+    pricerolex[3].innerHTML = "text provided by user";
+    pricerolex[4].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    console.log(pricerolex)
+}
+function changeImagesbtnClick5(){
+    rolexOyster[0].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[1].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[2].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[3].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[4].setAttribute('src','images/landscapeMode.jpg')
+    rolexOyster[5].setAttribute('src','images/landscapeMode.jpg')
+    rolexTxt[0].innerHTML = "text provided by user";
+    rolexTxt[1].innerHTML = "text provided by user";
+    rolexTxt[2].innerHTML = "text provided by user";
+    rolexTxt[4].innerHTML = "text provided by user";
+    rolexTxt[5].innerHTML = "text provided by user";
+    pricerolex[0].innerHTML = "text provided by user";
+    pricerolex[1].innerHTML = "text provided by user";
+    pricerolex[2].innerHTML = "text provided by user";
+    pricerolex[3].innerHTML = "text provided by user";
+    pricerolex[4].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    pricerolex[5].innerHTML = "text provided by user";
+    console.log(pricerolex)
+}
+
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.yourisp.com",
+//         Username : "username",
+//         Password : "password",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+
+// }
